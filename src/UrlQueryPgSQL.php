d93fe0938380d5e48dd84bcd816a919802c404f0
Sileno de Oliveira Brito <sobrito@i9corp.local>
2020-06-15 12:05:02 -0300

    Adicionado limite, remocao de escape para field

--- src/UrlQueryPgSQL.php
+++ src/UrlQueryPgSQL.php
@@ -3,6 +3,7 @@
 namespace Nerd4ever\UrlQuery\Sql\UrlQueryPgSQL;
 
 
+use Nerd4ever\UrlQuery\Model\UrlQuery;
 use Nerd4ever\UrlQuery\Sql\AbstractUrlQuerySql;
 
 class UrlQueryPgSQL extends AbstractUrlQuerySql
@@ -10,32 +11,32 @@ class UrlQueryPgSQL extends AbstractUrlQuerySql
 
     protected function urlQueryFilterBetweenToSql($field, $start, $end): ?string
     {
-        return sprintf('%s BETWEEN \'%s\' AND \'%s\'', pg_escape_string($field), pg_escape_string($start), pg_escape_string($end));
+        return sprintf('%s BETWEEN \'%s\' AND \'%s\'', $field, pg_escape_string($start), pg_escape_string($end));
     }
 
     protected function urlQueryFilterContainsToSql($field, $value): ?string
     {
-        return sprintf('%s LIKE \'%%%s%%\'', pg_escape_string($field), pg_escape_string($value));
+        return sprintf('%s LIKE \'%%%s%%\'', $field, pg_escape_string($value));
     }
 
     protected function urlQueryFilterEqualsToSql($field, $value): ?string
     {
-        return sprintf('%s = \'%s\'', pg_escape_string($field), pg_escape_string($value));
+        return sprintf('%s = \'%s\'', $field, pg_escape_string($value));
     }
 
     protected function urlQueryFilterFinishToSql($field, $value): ?string
     {
-        return sprintf('%s LIKE \'%%%s\'', pg_escape_string($field), pg_escape_string($value));
+        return sprintf('%s LIKE \'%%%s\'', $field, pg_escape_string($value));
     }
 
     protected function urlQueryFilterGreaterThanToSql($field, $value): ?string
     {
-        return sprintf('%s > \'%s\'', pg_escape_string($field), pg_escape_string($value));
+        return sprintf('%s > \'%s\'', $field, pg_escape_string($value));
     }
 
     protected function urlQueryFilterGreaterThanOrEqualsToSql($field, $value): ?string
     {
-        return sprintf('%s >= \'%s\'', pg_escape_string($field), pg_escape_string($value));
+        return sprintf('%s >= \'%s\'', $field, pg_escape_string($value));
     }
 
     protected function urlQueryFilterInToSql($field, array $values): ?string
@@ -48,32 +49,32 @@ class UrlQueryPgSQL extends AbstractUrlQuerySql
 
     protected function urlQueryFilterLessThanToSql($field, $value): ?string
     {
-        return sprintf('%s < \'%s\'', pg_escape_string($field), pg_escape_string($value));
+        return sprintf('%s < \'%s\'', $field, pg_escape_string($value));
     }
 
     protected function urlQueryFilterLessThanOrEqualsToSql($field, $value): ?string
     {
-        return sprintf('%s <= \'%s\'', pg_escape_string($field), pg_escape_string($value));
+        return sprintf('%s <= \'%s\'', $field, pg_escape_string($value));
     }
 
     protected function urlQueryFilterNilToSql($field): ?string
     {
-        return sprintf('%s IS NULL', pg_escape_string($field));
+        return sprintf('%s IS NULL', $field);
     }
 
     protected function urlQueryFilterNotEqualsToSql($field, $value): ?string
     {
-        return sprintf('%s <> \'%s\'', pg_escape_string($field), pg_escape_string($value));
+        return sprintf('%s <> \'%s\'', $field, pg_escape_string($value));
     }
 
     protected function urlQueryFilterRegexToSql($field, $value): ?string
     {
-        return sprintf('%s ~ \'%s\'', pg_escape_string($field), $value);
+        return sprintf('%s ~ \'%s\'', $field, $value);
     }
 
     protected function urlQueryFilterStartToSql($field, $value): ?string
     {
-        return sprintf('%s LIKE \'%s%%\'', pg_escape_string($field), pg_escape_string($value));
+        return sprintf('%s LIKE \'%s%%\'', $field, pg_escape_string($value));
     }
 
     public function urlQuerySort($field, $type): ?string
@@ -82,8 +83,13 @@ class UrlQueryPgSQL extends AbstractUrlQuerySql
     }
 
 
-    public function urlQueryLimitToSql(?int $limit, ?int $offset)
+    public function urlQueryLimitToSql(UrlQuery $urlQuery)
     {
-        // TODO: Implement urlQueryLimitToSql() method.
+        if ($urlQuery->getLimit() == null) return '';
+        $mSql = sprintf('LIMIT %d', intval($urlQuery->getLimit()));
+        if ($urlQuery->getOffset() != null) {
+            $mSql = sprintf(' OFFSET %d', intval($urlQuery->getOffset()));
+        }
+        return $mSql;
     }
 }
