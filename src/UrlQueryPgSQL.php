437a233a3132ee515476fe982fd2040087f01483
Sileno de Oliveira Brito <sobrito@i9corp.local>
2020-06-17 09:03:28 -0300

    Downgrade de versão do php

--- src/UrlQueryPgSQL.php
+++ src/UrlQueryPgSQL.php
@@ -5,56 +5,68 @@ namespace Nerd4ever\UrlQuery\Sql\UrlQueryPgSQL;
 
 use Nerd4ever\UrlQuery\Model\UrlQuery;
 use Nerd4ever\UrlQuery\Sql\AbstractUrlQuerySql;
+use PDO;
 
 class UrlQueryPgSQL extends AbstractUrlQuerySql
 {
 
+    private $pdo;
+
+    /**
+     * UrlQueryPgSQL constructor.
+     * @param PDO $pdo
+     */
+    public function __construct(?PDO $pdo = null)
+    {
+        $this->pdo = $pdo;
+    }
+
     protected function urlQueryFilterBetweenToSql($field, $start, $end): ?string
     {
-        return sprintf('%s BETWEEN \'%s\' AND \'%s\'', $field, pg_escape_string($start), pg_escape_string($end));
+        return sprintf('%s BETWEEN \'%s\' AND \'%s\'', $field, $this->quote($start), $this->quote($end));
     }
 
     protected function urlQueryFilterContainsToSql($field, $value): ?string
     {
-        return sprintf('%s LIKE \'%%%s%%\'', $field, pg_escape_string($value));
+        return sprintf('%s LIKE \'%%%s%%\'', $field, $this->quote($value));
     }
 
     protected function urlQueryFilterEqualsToSql($field, $value): ?string
     {
-        return sprintf('%s = \'%s\'', $field, pg_escape_string($value));
+        return sprintf('%s = \'%s\'', $field, $this->quote($value));
     }
 
     protected function urlQueryFilterFinishToSql($field, $value): ?string
     {
-        return sprintf('%s LIKE \'%%%s\'', $field, pg_escape_string($value));
+        return sprintf('%s LIKE \'%%%s\'', $field, $this->quote($value));
     }
 
     protected function urlQueryFilterGreaterThanToSql($field, $value): ?string
     {
-        return sprintf('%s > \'%s\'', $field, pg_escape_string($value));
+        return sprintf('%s > \'%s\'', $field, $this->quote($value));
     }
 
     protected function urlQueryFilterGreaterThanOrEqualsToSql($field, $value): ?string
     {
-        return sprintf('%s >= \'%s\'', $field, pg_escape_string($value));
+        return sprintf('%s >= \'%s\'', $field, $this->quote($value));
     }
 
     protected function urlQueryFilterInToSql($field, array $values): ?string
     {
         $in = array();
-        foreach ($values as $v) $in[] = pg_escape_string($v);
+        foreach ($values as $v) $in[] = $this->quote($v);
         $mValues = join("','", $in);
         return sprintf('%s IN (\'%s\')', $field, $mValues);
     }
 
     protected function urlQueryFilterLessThanToSql($field, $value): ?string
     {
-        return sprintf('%s < \'%s\'', $field, pg_escape_string($value));
+        return sprintf('%s < \'%s\'', $field, $this->quote($value));
     }
 
     protected function urlQueryFilterLessThanOrEqualsToSql($field, $value): ?string
     {
-        return sprintf('%s <= \'%s\'', $field, pg_escape_string($value));
+        return sprintf('%s <= \'%s\'', $field, $this->quote($value));
     }
 
     protected function urlQueryFilterNilToSql($field): ?string
@@ -64,7 +76,7 @@ class UrlQueryPgSQL extends AbstractUrlQuerySql
 
     protected function urlQueryFilterNotEqualsToSql($field, $value): ?string
     {
-        return sprintf('%s <> \'%s\'', $field, pg_escape_string($value));
+        return sprintf('%s <> \'%s\'', $field, $this->quote($value));
     }
 
     protected function urlQueryFilterRegexToSql($field, $value): ?string
@@ -74,7 +86,7 @@ class UrlQueryPgSQL extends AbstractUrlQuerySql
 
     protected function urlQueryFilterStartToSql($field, $value): ?string
     {
-        return sprintf('%s LIKE \'%s%%\'', $field, pg_escape_string($value));
+        return sprintf('%s LIKE \'%s%%\'', $field, $this->quote($value));
     }
 
     public function urlQuerySort($field, $type): ?string
@@ -91,5 +103,11 @@ class UrlQueryPgSQL extends AbstractUrlQuerySql
             $mSql = sprintf(' OFFSET %d', intval($urlQuery->getOffset()));
         }
         return $mSql;
+    }
+
+    private function quote($value)
+    {
+        if ($this->pdo != null) return $this->pdo->quote($value);
+        return addslashes($value);
     }
 }
